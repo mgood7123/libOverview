7f582cdedbe34154f7332643caf57940dd773063
mgood7123 <smallville7123@gmail.com>
2021-03-27 23:13:15 +1000

    update libs, reduce api from 30 to 29

--- src/test/java/smallville7123/liboverview/ExampleUnitTest.java
+++ src/test/java/smallville7123/liboverview/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package smallville7123.liboverview;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
